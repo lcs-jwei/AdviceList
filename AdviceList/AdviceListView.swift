9f29f538a512e87eda2ec35d90be924d7e6aad26
Justin <jwei@lcs.on.ca>
2023-04-20 13:30:33 -0400

    Fixed errors, made it so you can get new advice on the click of that button

--- AdviceList/AdviceListView.swift
+++ AdviceList/AdviceListView.swift
@@ -13,11 +13,14 @@ struct AdviceListView: View {
     var body: some View {
         NavigationView{
             VStack{
+                Image(systemName: "sun.max.fill")
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .foregroundColor(.yellow)
+                Spacer()
                 if let currentAdvice = currentAdvice{
-                    Image(systemName: "sun.max.fill")
-                                    .resizable()
-                                    .frame(width: 200, height: 200)
-                                    .foregroundColor(.yellow)
+                    
+                    
                                     
                     Text(currentAdvice.advice)
                         .font(.system(size: 30))
@@ -25,58 +28,54 @@ struct AdviceListView: View {
                         .opacity(adviceOpacity)
                                     
                       
-                    Button(action: {
-                        // Reset the interface
-                        adviceOpacity = 0.0
-
-                        Task {
-                            // Get another joke
-                            withAnimation {
-                                currentAdvice = nil
-                            }
-                            currentAdvice = await NetworkService.fetch()
-                        }
-                    }, label: {
-                        Text("Fetch another one")
-                    })
+                    
                     .disabled(adviceOpacity == 0.0 ? true : false)
                     .buttonStyle(.borderedProminent)
-                    /*
-                    Button(action: {
-                        withAnimation(.easeIn(duration: 1.0)){
-                            adviceOpacity = 1.0
-                        }
-                               
-                           }) {
-                               Text("Generate Some Advice")
-                                   .font(.headline)
-                                   .padding()
-                                   .foregroundColor(.white)
-                                   .background(Color.blue)
-                                   .cornerRadius(10)
-                           }*/
-                    Button(action: {
-                               
-                           }) {
-                               Text("Save Advice")
-                                   .font(.caption)
-                                   .bold()
-                                   .padding()
-                                   .foregroundColor(.white)
-                                   .background(Color.green)
-                                   .cornerRadius(10)
-                           }
+                    
+                    
                 }else {
                     ProgressView()
+                    Spacer()
                 }
+                Button(action: {
+                    // Reset the interface
+                    currentAdvice = nil
+
+                    Task {
+                        // Get another joke
+                        withAnimation {
+                            adviceOpacity = 1.0
+                        }
+                        currentAdvice = await NetworkService.fetch()
+                    }
+                }, label: {
+                    Text("Generate Some Advice")
+                        .font(.headline)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                Button(action: {
+                           
+                       }) {
+                           Text("Save Advice")
+                               .font(.caption)
+                               .bold()
+                               .padding()
+                               .foregroundColor(.white)
+                               .background(Color.green)
+                               .cornerRadius(10)
+                       }
 
                 
             }
            
                 .navigationTitle("Random Advice")
+                .task{
+                    currentAdvice = await NetworkService.fetch()
         }
-        .task{
-            currentAdvice = await NetworkService.fetch()
+        
         }
     }
 }
