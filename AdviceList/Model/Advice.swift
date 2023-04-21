7d93fa8a31c5e47c4dc9d0bc99db995e55645217
Justin <jwei@lcs.on.ca>
2023-04-21 15:54:00 -0400

    Adjusted the model so that a joke can be read from or written to the database.

--- AdviceList/Model/Advice.swift
+++ AdviceList/Model/Advice.swift
@@ -5,11 +5,13 @@
 //  Created by Justin Zack Wei on 2023-04-19.
 //
 
+import Blackbird
 import Foundation
 
-struct Advice: Identifiable, Codable{
-    let advice: String
-    let id: Int
+
+struct Advice: Identifiable, Codable, BlackbirdModel{
+    @BlackbirdColumn var advice: String
+    @BlackbirdColumn var id: Int
 }
 
 let exampleAdvice = Advice(advice: "Some of life's best lessons are learnt at the worst times.", id: 164)
